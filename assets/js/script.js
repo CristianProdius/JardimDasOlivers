a07c610a052ef21ec2230b374e513d907b4ac863
Cristian Prodius <m318572@alunos.ipb.pt>
2023-12-20 06:05:52 +0000

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -95,3 +95,28 @@ const scrollReveal = function () {
 scrollReveal();
 
 addEventOnElem(window, "scroll", scrollReveal);
+
+
+/*
+  Inventory FIlter
+*/
+let filterItem = document.querySelector('.item-links');
+let filterImages = document.querySelectorAll('.inventroy-item');
+
+window.addEventListener('load',()=>{
+  filterItem.addEventListener('click',(selectedItem)=>{
+    if(selectedItem.target.classList.contains('item-link')){
+      document.querySelector('.menu-active').classList.remove('menu-active');
+      selectedItem.target.classList.add('menu-active');
+      let filterName = selectedItem.target.getAttribute('data-name');
+      filterImages.forEach((image)=>{
+        let filterImages = image.getAttribute('data-name');
+        if((filterImages == filterName) || filterName == 'all'){
+          image.style.display = 'block'
+        } else{
+          image.style.display = 'none'
+        }
+      })
+    }
+  })
+})
